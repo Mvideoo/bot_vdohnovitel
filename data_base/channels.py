4bc1c074c3eae3a965b6320aac13c3c43a1f8569
Mvideoo <132948904+Mvideoo@users.noreply.github.com>
2024-06-13 01:24:12 +0300

    Add files via upload

---
+++ data_base/channels.py
@@ -0,0 +1,6 @@
+technician = ['https://t.me/positronph', 'https://t.me/Education_Yandex', '']
+bio_chemical = ['https://t.me/ChemistryEasy', '']
+social_economy = ['']
+language = ['']
+
+all_channels = ['https://t.me/vzletolympmo', 'https://t.me/sirius_notification']
